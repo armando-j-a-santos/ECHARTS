89417007af5196c1ed21d32ddc5f16be6299ed15
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-09-21 16:04:17 +0100

    Update and rename xy31.js to xy32.js

--- originalcode/xy32.js
+++ originalcode/xy32.js
@@ -2253,13 +2253,6 @@
                         configurable: !0,
                         writable: !0,
                         value: function(e, t) {}
-                    }), Object.defineProperty(t.prototype, "_clearDirty", {
-                        enumerable: !1,
-                        configurable: !0,
-                        writable: !0,
-                        value: function() {
-                            e.prototype._clearDirty.call(this), this._sizeDirty = !1, this._rangesDirty = !1
-                        }
                     }), Object.defineProperty(t.prototype, "coordinateToPosition", {
                         enumerable: !1,
                         configurable: !0,
@@ -3452,13 +3445,6 @@
                                 })), e._dataSetId && this.markDirtySize()
                             }
                         }
-                    }), Object.defineProperty(t.prototype, "_clearDirty", {
-                        enumerable: !1,
-                        configurable: !0,
-                        writable: !0,
-                        value: function() {
-                            e.prototype._clearDirty.call(this), this._groupingCalculated = !1, this._dataGrouped = !1
-                        }
                     }), Object.defineProperty(t.prototype, "getGroupInterval", {
                         enumerable: !1,
                         configurable: !0,
@@ -3592,7 +3578,7 @@
                                 }
                                 for (var O = p; O < this.dataItems.length; O++) this.dataItems[O].hide();
                                 l.each(this.series, (function(e) {
-                                    e.inited && e._markDirtyAxes()
+                                    e.inited /////////////&& e._markDirtyAxes()
                                 }))
                             }
                             this._updateGhost()
@@ -3954,7 +3940,7 @@
                                     }))
                                 }
                                 o.each(this.series, (function(e) {
-                                    e.inited && e._markDirtyAxes()
+                                    e.inited /////////////&& e._markDirtyAxes()
                                 }))
                             }
                             this._updateGhost()
@@ -4146,7 +4132,7 @@
                                 }
                                 for (var y = c; y < this.dataItems.length; y++) this.dataItems[y].hide();
                                 l.each(this.series, (function(e) {
-                                    e.inited && e._markDirtyAxes()
+                                    e.inited /////////////&& e._markDirtyAxes()
                                 })), this._updateGhost()
                             }
                         }
@@ -6059,20 +6045,6 @@
                         value: function(e) {
                             this.updateLegendMarker()
                         }
-                    }), Object.defineProperty(t.prototype, "_markDirtyAxes", {
-                        enumerable: !1,
-                        configurable: !0,
-                        writable: !0,
-                        value: function() {
-                            this._axesDirty = !0, this.markDirty()
-                        }
-                    }),Object.defineProperty(t.prototype, "_clearDirty", {
-                        enumerable: !1,
-                        configurable: !0,
-                        writable: !0,
-                        value: function() {
-                            e.prototype._clearDirty.call(this), this._axesDirty = !1, this._selectionProcessed = !1, this._stackDirty = !1, this._dataProcessed = !1
-                        }
                     }), Object.defineProperty(t.prototype, "_positionBullet", {         // needed in 4604
                         enumerable: !1,
                         configurable: !0,
@@ -6345,8 +6317,8 @@
                                     n = i.get("baseAxis"),
                                     o = i.get("xAxis"),
                                     s = i.get("yAxis");
-                                if (o instanceof t && s instanceof t) i._markDirtyAxes();
-                                else if (n === e) {
+                                /////////////if (o instanceof t && s instanceof t) i._markDirtyAxes();
+                                if (n === e) {
                                     var l = void 0,
                                         u = void 0,
                                         h = s;
@@ -6387,7 +6359,7 @@
                                         for (d = m; d < v && "break" !== _(d); d++);
                                         i.setPrivate("outOfSelection", !y)
                                     }
-                                    i._markDirtyAxes()
+                                    /////////////i._markDirtyAxes()
                                 }
                             }))
                         }
