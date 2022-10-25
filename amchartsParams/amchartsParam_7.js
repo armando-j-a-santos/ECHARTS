82751cf21a54c059d7130d0f99aea161bc291560
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-10-25 17:05:42 +0100

    commit

--- amchartsParams/amchartsParam_7.js
+++ amchartsParams/amchartsParam_7.js
@@ -26,7 +26,7 @@
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
   // HTML extension with all necessary logic(s) wrtitten JS vvvvvvvvvvvv
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv  
-  class amchartsParams_18 extends HTMLElement {
+  class amchartsParams_19 extends HTMLElement {
     constructor () {
       super()
 
@@ -124,7 +124,7 @@
 
         // #############
             // Variables definition
-            var CAct_Dark = x;
+            var CAct_Dark = am4core.color(myConfig.CAct_Dark);
             var CAct_Light =  "#68a0cb";
 
             var CNonAct_Col1_Dark = "#776644";
@@ -502,6 +502,6 @@
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
   // Return the end result to SAC (SAP ANALYTICS CLOUD) application vvvvvvvvvvvvvvvvvvvvv
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-  customElements.define('com-sap-sample-asantos-amchartspar', amchartsParams_18)
+  customElements.define('com-sap-sample-asantos-amchartspar', amchartsParams_19)
   
 })() // END of function --> (function () {
